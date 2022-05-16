49aaeeb9c15c7e260c260a7679c33e10e59385c4
Rishabh Chopra <rishabhchopra68@gmail.com>
2022-05-16 16:59:02 +0530

    navbar styles changed

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,30 +1,26 @@
 import { useContext } from "react";
 import "./works.scss";
 
-
 import Card from "../Card/Card";
-import {PortfolioContext} from './../../context/PortfolioContext';
+import { PortfolioContext } from "./../../context/PortfolioContext";
 
 export default function Works() {
-  const {projects} = useContext(PortfolioContext);
+  const { projects } = useContext(PortfolioContext);
   console.log(projects);
 
   return (
-    <div 
-      className="work"
-      id="work"
-    >
-      <h1>Works Page</h1>
+    <div className="work" id="work">
+      <h1>Sample Work</h1>
       <div className="works-container">
-        {projects.map((project) => 
-          <Card 
+        {projects.map((project) => (
+          <Card
             key={project.name}
             image={project.image}
             name={project.name}
             projectlink={project.projectlink}
             codelink={project.codelink}
-        />
-        )}
+          />
+        ))}
       </div>
     </div>
   );
